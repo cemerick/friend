1618a4ad002317f2574cde0c05b1aa705e2d9c77
ken restivo <krestivo@restivo.org>
2012-06-05 11:40:36 -0700

    Stash modified :session in metadata of auth-map of workflow, and restore it in the merge-authentication step at the core abstraction.

--- src/cemerick/friend.clj
+++ src/cemerick/friend.clj
@@ -44,10 +44,13 @@
                                   :server-port (scheme-mapping scheme))))))))
 
 (defn merge-authentication
+  "If the workflow has modified the session and passed it in meta, use it.
+  Then update the authentications."
   [m auth]
-  (update-in m [:session ::identity]
-             #(-> (assoc-in % [:authentications (:identity auth)] auth)
-                (assoc :current (:identity auth)))))
+  (let [new-session (or (-> auth meta :session) (:session m))]
+    (assoc m :session (update-in new-session [::identity]
+                              #(-> (assoc-in % [:authentications (:identity auth)] auth)
+                                   (assoc :current (:identity auth)))))))
 
 (defn- logout*
   [response]
