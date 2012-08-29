4814229254f59e6538dcf406f9173a7443556d02
Chas Emerick <chas@cemerick.com>
2012-08-29 16:10:10 -0400

    add support for credential function to return maps with custom password key

--- src/cemerick/friend/credentials.clj
+++ src/cemerick/friend/credentials.clj
@@ -18,8 +18,25 @@
 
    (authenticate {:credential-fn (partial bcrypt-credential-fn load-user-record)
                   :other :config ...}
-     ring-handler-to-be-secured)"
+     ring-handler-to-be-secured)
+
+   The credentials map returned by the provided function will only be returned if
+   the provided (cleartext, user-supplied) password matches the hashed
+   password in the credentials map.
+
+   The password in the credentials map will be looked up using a :password
+   key by default; if the credentials-loading function will return a credentials
+   map that stores the hashed password under a different key, it must specify
+   that alternative key via a :cemerick.friend.credentials/password-key slot
+   in the map's metadata.  So, if a credentials map looks like:
+
+     {:username \"joe\" :app.foo/passphrase \"bcrypt hash\"}
+
+   ...then the hash will be verified correctly as long as the credentials
+   map contains a [:cemerick.friend.credentials/password-key :app.foo/passphrase]
+   entry."
   [load-credentials-fn {:keys [username password]}]
   (when-let [creds (load-credentials-fn username)]
-    (when (BCrypt/checkpw password (:password creds))
-      (dissoc creds :password))))
+    (let [password-key (or (-> creds meta ::password-key) :password)]
+      (when (BCrypt/checkpw password (get creds password-key))
+        (dissoc creds password-key)))))
---
+++ test/test_friend/credentials.clj
@@ -0,0 +1,21 @@
+(ns test-friend.credentials
+  (:require [cemerick.friend.credentials :as creds])
+  (:use clojure.test))
+
+(deftest simple
+  (is (= {:username "joe"}
+         (creds/bcrypt-credential-fn
+           {"username" {:username "joe" :password (creds/hash-bcrypt "foo")}}
+           {:username "username" :password "foo"}))))
+
+(deftest custom-password-key
+  (is (thrown? NullPointerException
+              (creds/bcrypt-credential-fn
+                {"username" {"username" "joe" ::password (creds/hash-bcrypt "foo")}}
+                {:username "username" :password "foo"})))
+  
+  (is (= {:username "joe"}
+         (creds/bcrypt-credential-fn
+           {"username" ^{::creds/password-key ::password}
+                        {:username "joe" ::password (creds/hash-bcrypt "foo")}}
+           {:username "username" :password "foo"}))))
