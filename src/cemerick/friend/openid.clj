af3ab2468ff51c0332718584f03321ad3f97b790
Tavis Rudd <tavis@damnsimple.com>
2013-09-16 14:00:02 -0700

    fix whitespace issues
    rm some tabs and trailing whitespace prior to a less annoying ;) patch to fix a proxy issue

--- src/cemerick/friend/openid.clj
+++ src/cemerick/friend/openid.clj
@@ -17,10 +17,10 @@
 
 (def ^{:private true} ax-props
   {"country" "http://axschema.org/contact/country/home"
-   "email"	"http://axschema.org/contact/email"  ;"http://schema.openid.net/contact/email".
+   "email" "http://axschema.org/contact/email"  ;"http://schema.openid.net/contact/email".
    "firstname" "http://axschema.org/namePerson/first"
-   "language"	"http://axschema.org/pref/language"
-   "lastname"	"http://axschema.org/namePerson/last"})
+   "language" "http://axschema.org/pref/language"
+   "lastname" "http://axschema.org/namePerson/last"})
 
 (def ^{:private true} sreg-attrs
   ["nickname", "email", "fullname", "dob",
@@ -70,7 +70,7 @@
                                         type (.getOpEndpoint response)))
                        (.printStackTrace e)))]
       (when (instance? ext-class ext)  ;; is this ever necessary? yanked from the examples...
-        (->> attr-keys 
+        (->> attr-keys
           (map #(when-let [v (.getAttributeValue ext %)] [(keyword %) v]))
           (into ^{:type type} {}))))))
 
@@ -113,7 +113,7 @@
             user-identifier
             (handle-init mgr discovery-cache user-identifier request
                          (gets :realm openid-config (::friend/auth-config request)))
-            
+
             (contains? params return-key)
             (if-let [auth-map (handle-return mgr discovery-cache
                                              (assoc request :params params) openid-config)]
@@ -122,7 +122,7 @@
               ((or (gets :login-failure-handler openid-config (::friend/auth-config request))
                    #'workflows/interactive-login-redirect)
                 (update-in request [::friend/auth-config] merge openid-config)))
-            
+
             ;; TODO correct response code?
             :else ((gets :login-failure-handler openid-config (::friend/auth-config request))
                     request)))))))
