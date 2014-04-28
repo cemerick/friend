b464335cadd92530f2a15cdc3d286cf2bd41a5da
Brent Millare <brent.millare@gmail.com>
2014-04-28 15:25:53 -0400

    dependencies simplification

--- project.clj
+++ project.clj
@@ -1,44 +1,27 @@
-(defproject com.cemerick/friend "0.2.1-SNAPSHOT"
-  :description "Authentication and authorization library for Ring Clojure web apps and services."
-  :url "http://github.com/cemerick/friend"
+(defproject org.bmillare/friend "0.1.0"
+  :description "Authentication and authorization library for Ring Clojure web apps and services, decoupled, forked from cemerick/friend"
+  :url "http://github.com/bmillare/friend"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring/ring-core "1.2.0"]
-                 [slingshot "0.10.2"]
-                 [robert/hooke "1.1.2"]
+  :dependencies [[ring/ring-core "1.2.1"]
+                 [slingshot "0.10.3"]
                  
-                 [org.mindrot/jbcrypt "0.3m"]
+                 ;; [org.mindrot/jbcrypt "0.3m"]
 
                  ;; http-basic
                  [commons-codec "1.6"]
 
                  ;; openid
-                 [org.clojure/core.cache "0.6.2"]
-                 [org.openid4java/openid4java-nodeps "0.9.6"
+                 #_ [org.clojure/core.cache "0.6.2"]
+                 #_ [org.openid4java/openid4java-nodeps "0.9.6"
                   ; the openid4java artifact refers to a now-disappeared guice repo that
                   ; was previously hosted via google code svn :X
-                  :exclusions [com.google.code.guice/guice]]
-                 [com.google.inject/guice "2.0"]
-                 [net.sourceforge.nekohtml/nekohtml "1.9.10"]
+                 #_ #_  :exclusions [com.google.code.guice/guice]]
+                 #_ [com.google.inject/guice "2.0"]
+                 #_ [net.sourceforge.nekohtml/nekohtml "1.9.10"]
                  ; need different httpclient rev for https://issues.apache.org/jira/browse/HTTPCLIENT-1118
-                 [org.apache.httpcomponents/httpclient "4.2.1"]]
-  
-  :deploy-repositories {"releases" {:url "https://clojars.org/repo/" :creds :gpg}
-                        "snapshots" {:url "https://clojars.org/repo/" :creds :gpg}}
-  
-  :profiles {:dev {:dependencies [[ring-mock "0.1.1"]
-                                  [compojure "1.1.5"]
-                                  [ring "1.2.0"]
-                                  [clj-http "0.3.6"]]}
-             :sanity-check {:aot :all
-                            :warn-on-reflection true
-                            :compile-path "target/sanity-check-aot"}
-             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
-  :aliases  {"all" ["with-profile" "dev,1.2:dev,1.3:dev:dev,1.5"]
-             "sanity-check" ["do" "clean," "with-profile" "sanity-check" "compile"]})
+                 #_ [org.apache.httpcomponents/httpclient "4.2.1"]])
 
 ;; see:
 ;; http://static.springsource.org/spring-security/site/docs/3.1.x/reference/springsecurity-single.html#overall-architecture
