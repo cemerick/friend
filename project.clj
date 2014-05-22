5fc0a357058cd96e35dd231fc47a31e65cb01031
Chas Emerick <chas@cemerick.com>
2014-05-22 14:39:29 -0400

    0.2.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.cemerick/friend "0.2.1"
+(defproject com.cemerick/friend "0.2.2-SNAPSHOT"
   :description "Authentication and authorization library for Ring Clojure web apps and services."
   :url "http://github.com/cemerick/friend"
   :license {:name "Eclipse Public License"
