926f01ba718c355c82faad4aa0438162708a1efb
Plínio Balduino <pbalduino+github@gmail.com>
2014-11-06 10:59:17 -0200

    Changed friend version from 0.2.2-SNAPSHOT to 0.2.3-SNAPSHOT
    Updated versions of third-party libraries:
    - ring-core 1.2.0 -> 1.3.1
    - slingshot 0.10.2 -> 0.12.1
    - commons-codec 1.6 -> 1.9
    - core.cache 0.6.2 -> 0.6.4
    - ring-mock 0.1.1 -> 0.1.5
    - compojure 1.1.5 -> 1.2.1
    - ring 1.2.0 -> 1.3.1
    - clj-http 0.3.6 1.0.1

--- project.clj
+++ project.clj
@@ -1,20 +1,20 @@
-(defproject com.cemerick/friend "0.2.2-SNAPSHOT"
+(defproject com.cemerick/friend "0.2.3-SNAPSHOT"
   :description "Authentication and authorization library for Ring Clojure web apps and services."
   :url "http://github.com/cemerick/friend"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring/ring-core "1.2.0"]
-                 [slingshot "0.10.2"]
+                 [ring/ring-core "1.3.1"]
+                 [slingshot "0.12.1"]
 
                  [org.mindrot/jbcrypt "0.3m"]
 
                  ;; http-basic
-                 [commons-codec "1.6"]
+                 [commons-codec "1.9"]
 
                  ;; openid
-                 [org.clojure/core.cache "0.6.2"]
+                 [org.clojure/core.cache "0.6.4"]
                  [org.openid4java/openid4java-nodeps "0.9.6"
                   ; the openid4java artifact refers to a now-disappeared guice repo that
                   ; was previously hosted via google code svn :X
@@ -27,17 +27,15 @@
   :deploy-repositories {"releases" {:url "https://clojars.org/repo/" :creds :gpg}
                         "snapshots" {:url "https://clojars.org/repo/" :creds :gpg}}
   
-  :profiles {:dev {:dependencies [[ring-mock "0.1.1"]
-                                  [compojure "1.1.5"]
-                                  [ring "1.2.0"]
+  :profiles {:dev {:dependencies [[ring-mock "0.1.5"]
+                                  [compojure "1.2.1"]
+                                  [ring "1.3.1"]
                                   [robert/hooke "1.3.0"]
-                                  [clj-http "0.3.6"]]}
+                                  [clj-http "1.0.1"]]}
              :sanity-check {:aot :all
                             :warn-on-reflection true
-                            :compile-path "target/sanity-check-aot"}
-             :1.5 [:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}]}
-  :aliases  {"all" ["with-profile" "1.5:dev"]
-             "sanity-check" ["do" "clean," "with-profile" "sanity-check" "compile"]})
+                            :compile-path "target/sanity-check-aot"}}
+  :aliases  {"sanity-check" ["do" "clean," "with-profile" "sanity-check" "compile"]})
 
 ;; see:
 ;; http://static.springsource.org/spring-security/site/docs/3.1.x/reference/springsecurity-single.html#overall-architecture
