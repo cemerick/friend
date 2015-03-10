e1184bda84116afd8d422ab2695503830537959b
ben lamothe <zonotope@gmail.com>
2015-03-09 20:04:43 -0400

    add bcrypt suffixes to tests.

--- test/test_friend/credentials.clj
+++ test/test_friend/credentials.clj
@@ -2,13 +2,13 @@
   (:require [cemerick.friend.credentials :as creds])
   (:use clojure.test))
 
-(deftest simple
+(deftest simple-bcrypt
   (is (= {:username "joe"}
          (creds/bcrypt-credential-fn
           {"username" {:username "joe" :password (creds/hash-bcrypt "foo")}}
           {:username "username" :password "foo"}))))
 
-(deftest custom-password-key
+(deftest custom-password-key-bcrypt
   (is (thrown? NullPointerException
                (creds/bcrypt-credential-fn
                 {"username" {"username" "joe"
