3b83a6b4a0943efa1b3f634125b6ee9c04e2f55b
ben lamothe <zonotope@gmail.com>
2015-03-09 20:09:50 -0400

    test pbkdf2 credential fn.

--- test/test_friend/credentials.clj
+++ test/test_friend/credentials.clj
@@ -20,3 +20,10 @@
           {"username" ^{::creds/password-key ::password}
            {:username "joe" ::password (creds/hash-bcrypt "foo")}}
           {:username "username" :password "foo"}))))
+
+
+(deftest simple-pbkdf2
+  (is (= {:username "joe"}
+         (creds/pbkdf2-credential-fn
+          {"username" {:username "joe" :password (creds/hash-pbkdf2 "foo")}}
+          {:username "username" :password "foo"}))))
