5a5791864847108e5facbfa34d77dc3dfa567c1b
ben lamothe <zonotope@gmail.com>
2015-03-12 18:02:47 -0400

    docstring for pbkdf2-credential-fn

--- src/cemerick/friend/credentials.clj
+++ src/cemerick/friend/credentials.clj
@@ -80,7 +80,31 @@
   (pbkdf2/check password hash))
 
 (def pbkdf2-credential-fn
-  ""
+  "A pbkdf2 credentials function intended to be used with
+  `cemerick.friend/authenticate` or individual authentication
+  workflows. You must supply a function of one argument that will look
+  up stored user credentials given a username/id. e.g.:
+
+    (authenticate {:credential-fn (partial pbkdf2-credential-fn load-user-record)
+                   :other :config ...}
+       ring-handler-to-be-secured)
+
+  If the provided (cleartext, user-supplied) password matches the
+  hashed password in the credentials map, that map is returned.
+
+  The password in the credentials map will be looked up using
+  a :password key by default; if the credentials-loading function will
+  return a credentials map that stores the hashed password under a
+  different key, it must specify that alternative key via
+  a :cemerick.friend.credentials/password-key slot in the map's
+  metadata.  So, if a credentials map looks like:
+
+  {:username \"joe\" :app.foo/passphrase \"bcrypt hash\"}
+
+  ...then the hash will be verified correctly as long as the
+  credentials map contains
+  a [:cemerick.friend.credentials/password-key :app.foo/passphrase]
+  entry."
   (build-credential-fn pbkdf2-verify))
 
 (defn hash-scrypt
